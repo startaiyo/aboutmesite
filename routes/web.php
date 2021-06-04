dae071ece104bc3ea75a90406d82e0f91c5fa1f6
startaiyo <startaiyo0104@gmail.com>
2021-06-05 07:44:27 +0900

    vueとlaravelの接続完了

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::get('/{any}', function() {
+    return view('app');
+})->where('any', '.*');
 Route::get('blogs/blog','App\Http\Controllers\BlogController@index');
