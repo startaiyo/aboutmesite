c11f7389e9b0808dd63925832a953b03dced128f
startaiyo <startaiyo0104@gmail.com>
2021-06-08 08:22:19 +0900

    dateの表示

--- resources/js/views/Blogs.vue
+++ resources/js/views/Blogs.vue
@@ -5,7 +5,7 @@
         <router-link  class="btn btn-primary" to="/blogs/create">作成</router-link>
         <li
         v-for="blog in blogs"
-        v-bind:key="blog.id">{{blog.text}}
+        v-bind:key="blog.id">作成日時:{{formatDate(blog.created_at)}} {{blog.text}}
         <router-link :to ="`/blogs/${blog.id}`">detail</router-link>
         <router-link :to ="`/blogs/${blog.id}/edit`">edit</router-link>
         <span class="btn btn-danger" @click="blogsDelete(index, blog.id)">削除</span>
@@ -14,6 +14,7 @@
 </div>
 </template>
 <script>
+import moment from "moment"
 export default {
     data(){
         return{
@@ -28,6 +29,9 @@ export default {
                 this.$router.go('blogs')
             })
             .catch(e=>{console.log(e)});
+        },
+        formatDate(date){
+            if (!!date) return moment(date).format("YYYY年MM月DD日 dddd hh:mm:ss")
         }
     },
     created(){
