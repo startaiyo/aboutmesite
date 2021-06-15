af2f07a95918cd9f61fb5f88acdf8a95895450f8
startaiyo <startaiyo0104@gmail.com>
2021-06-16 08:17:46 +0900

    全ページにメニューバー表示

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -15,8 +15,7 @@
 <body>
 <div id="app">
     <div id = "nav">
-      <router-link to = "/">Home</router-link>
-      <router-link to = "/blogs">blog</router-link>
+      <navbar></navbar>
       <router-view/>
     </div>
 </div>
