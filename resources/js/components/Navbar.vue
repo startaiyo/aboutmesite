2f10e089fa3e1bd4b9c1cfac5419edfedf42d3dc
startaiyo <startaiyo0104@gmail.com>
2021-06-17 08:04:21 +0900

    works追加

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -17,23 +17,11 @@
         ></v-img>
       </template>
 
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
 
       <v-app-bar-title style="font-size:50px">Startaiyo's Portfolio</v-app-bar-title>
 
       <v-spacer></v-spacer>
-
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
-      </v-btn>
 
       <template v-slot:extension>
         <v-tabs align-with-title>
